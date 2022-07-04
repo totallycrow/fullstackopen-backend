ee2be2561ea624c87889991976750d2aa4a8d5e4
Adrian Gajda <adrian.gajda@hotmail.com>
2022-07-04 06:58:43 +0100

    exercise

--- index.js
+++ index.js
@@ -3,26 +3,28 @@ const app = express()
 
 app.use(express.json())
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2022-05-30T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2022-05-30T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2022-05-30T19:20:14.298Z",
-      important: true
-    }
-  ]
+let persons =[
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
 
   app.use(express.json())
 
@@ -30,8 +32,8 @@ let notes = [
     response.send('<h1>Hello World!</h1>')
   })
   
-  app.get('/api/notes', (request, response) => {
-    response.json(notes)
+  app.get('/api/persons', (request, response) => {
+    response.json(persons)
   })
 
   app.get('/api/notes/:id', (request, response) => {
