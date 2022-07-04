c47f5125ed4b3a194e795cfff88fbd4aa9e25485
Adrian Gajda <adrian.gajda@hotmail.com>
2022-07-04 14:38:42 +0100

    work on exercise

--- index.js
+++ index.js
@@ -1,88 +1,88 @@
-const express = require('express')
-const app = express()
+const { response } = require("express");
+const express = require("express");
+const app = express();
+const PORT = "3001";
 
-app.use(express.json())
-
-let persons =[
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
   },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
   },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
   },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from server");
+});
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+app.get("/info", (req, res) => {
+  const timeStamp = new Date(Date.now());
+  const personsNum = persons.length;
+
+  res.send(
+    `Phonebook has info for ${personsNum} people. <div>${timeStamp}</div>`
+  );
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const responsePerson = persons.find(
+    (person) => person.id === Number(req.params.id)
+  );
+  responsePerson ? res.json(responsePerson) : res.sendStatus(404);
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  persons = persons.filter((person) => person.id !== Number(req.params.id));
+
+  res.sendStatus(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+  const personId = Math.floor(Math.random() * 1000000000000000);
+
+  const newPerson = {
+    id: personId,
+    name: req.body.name || "",
+    number: req.body.number || "",
+  };
+
+  if (newPerson.name === "") {
+    return res.status(400).send("Name must not be empty").end();
+  } else if (newPerson.number === "") {
+    return res.status(400).send("Number must not be empty").end();
+  } else if (persons.find((person) => person.name === newPerson.name)) {
+    return res.status(400).send("Name already exists").end();
   }
-]
+  // ) {
+  //   return res.status(400).send("Test").end();
+  // }
+  // persons = persons.concat(newPerson);
+  // res.sendStatus(200);
 
-  app.use(express.json())
+  res.json(newPerson);
+});
 
-  app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-  })
-  
-  app.get('/api/persons', (request, response) => {
-    response.json(persons)
-  })
-
-  app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-
-      if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
-  })
-
-  app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-  
-    response.status(204).end()
-  })
-
-  const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-    return maxId + 1
-  }
-  
-  app.post('/api/notes', (request, response) => {
-    const body = request.body
-  
-    if (!body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
-    }
-  
-    const note = {
-      content: body.content,
-      important: body.important || false,
-      date: new Date(),
-      id: generateId(),
-    }
-  
-    notes = notes.concat(note)
-  
-    response.json(note)
-  })
-  
-  const PORT = 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
