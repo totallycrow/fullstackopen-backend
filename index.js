a4c21217f3ab85557e0fcbb07c9d9078e2c5d5ea
Adrian Gajda <adrian.gajda@hotmail.com>
2022-07-05 12:35:12 +0100

    code along

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 let notes = [
@@ -85,7 +87,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
