e360b3af486f595a968554ae409d8d479cb39596
Adrian Gajda <adrian.gajda@hotmail.com>
2022-07-07 15:22:41 +0100

    work

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
+app.use(express.static("build"));
 var morgan = require("morgan");
 morgan.token("person", function (req, res) {
   const newPerson = {
